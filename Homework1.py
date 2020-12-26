b89ef9a3eae23ec5965a4f0693592918ffd635e6
aycileks <45665396+aycileks@users.noreply.github.com>
2020-12-26 15:46:20 +0100

    init

---
+++ Homework1.py
@@ -0,0 +1,7 @@
+# GlobalAIHub Python Introduction Homework1
+
+num = 1
+while num < 6:
+    value = str(input("Please enter the value {}/5 : ".format(num)))
+    print(f"You entered {value} ")
+    num += 1
